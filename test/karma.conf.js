8aada12a458c5a7cc20fe7b2b754287875672a48
kzhou <bravecircle@gmail.com>
2016-11-25 00:30:21 -0500

    2.1

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -34,6 +34,7 @@ module.exports = function(config) {
       'bower_components/angular-material/angular-material.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-material-sidenav/angular-material-sidenav.js',
+      'bower_components/svg-assets-cache.js/svg-assets-cache.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "../angular-material-sidenav.js",
